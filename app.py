fc98532cf2620ac79145f211928c59003e5c9a3c
Hossam Megahed <hosammegahed91@gmail.com>
2022-06-10 09:45:23 +0400

    completing course first chapter

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('home.html', company='DIEZ')
+    return render_template('home.html')
 
 
 
-@app.route('/about')
-def about():
-    return 'This is a url shortner !'
+@app.route('/you-url', methods = ['GET', 'POST'])
+def you_url():
+
+    if request.method == 'POST':
+        return render_template('you_url.html', code=request.form['code'])
+    else:
+        return 'This is not valid'
